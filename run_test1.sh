d32cdfbb74f606eb96c04b1b3717ce17760d8c9f
gmezan <meza.gustavo@pucp.edu.pe>
2022-02-08 00:40:17 -0500

    initial commit

---
+++ run_test1.sh
@@ -0,0 +1,5 @@
+mpirun -np 1 bin/bt-mz.A.1 > results/bt-mz.A.1
+mpirun -np 2 bin/bt-mz.A.2 > results/bt-mz.A.2
+mpirun -np 4 bin/bt-mz.A.4 > results/bt-mz.A.4
+mpirun -np 8 bin/bt-mz.A.8 > results/bt-mz.A.8
+mpirun -np 16 bin/bt-mz.A.16 > results/bt-mz.A.16
---
+++ run_test2.sh
@@ -0,0 +1,5 @@
+mpirun -np 1 bin/bt-mz.B.1 > results/bt-mz.B.1
+mpirun -np 2 bin/bt-mz.B.2 > results/bt-mz.B.2
+mpirun -np 4 bin/bt-mz.B.4 > results/bt-mz.B.4
+mpirun -np 8 bin/bt-mz.B.8 > results/bt-mz.B.8
+mpirun -np 16 bin/bt-mz.B.16 > results/bt-mz.B.16
