944c0ed5c35fd59fa074741cdd048b457632583f
Gustavo <meza.gustavo@pucp.edu.pe>
2022-03-14 18:35:58 +0000

    new results

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,12 @@ bash /home/ubuntu/Desktop/sharedfolder/scripts/ovs_set_net.sh
 bash start_distributed_test.sh ovs W
 bash start_distributed_test.sh ovs A
 bash start_distributed_test.sh ovs B
+bash start_distributed_test.sh ovs C
 
 bash /home/ubuntu/Desktop/sharedfolder/scripts/sriov_set_net.sh
 
 bash start_distributed_test.sh sriov W
 bash start_distributed_test.sh sriov A
 bash start_distributed_test.sh sriov B
+bash start_distributed_test.sh sriov C
 
